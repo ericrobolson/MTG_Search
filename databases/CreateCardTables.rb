fd3af46365bab45888156ac3e5a264b8403ccaa0
Eric R Olson <eric.rob.olson@gmail.com>
2016-09-11 14:36:17 -0700

    Finalized Database Creation scripts to update linking tables and values.

---
+++ databases/CreateCardTables.rb
@@ -0,0 +1,171 @@
+# Eric Olson (c) 2016
+# Parse the JSON file containing infomation about 
+# the Magic The Gathering cards into databases
+
+require 'JSON'
+require 'sqlite3'
+DATABASE_LOCATION = ''
+
+#####################
+# Create the Tables #
+#####################
+cardInformationDb = DATABASE_LOCATION + 'CardInformation.db'
+SQLite3::Database.new(cardInformationDb) do |db|
+	# Create the table containing sets
+	db.execute("CREATE TABLE IF NOT EXISTS CardSet(
+		SetId INTEGER PRIMARY KEY ASC,
+		Name TEXT NOT NULL,
+		Code TEXT NOT NULL,
+		ReleaseDate TEXT NOT NULL,
+		Border TEXT NOT NULL,
+		Type TEXT NOT NULL,
+		Booster TEXT NOT NULL,
+		CONSTRAINT Unique_Set
+		UNIQUE (Name, Code, ReleaseDate, Border, Type, Booster)
+	);
+	")
+
+	# Create the table containing cards
+	db.execute("CREATE TABLE IF NOT EXISTS Card(
+		CardId INTEGER PRIMARY KEY ASC,
+		Name TEXT NOT NULL,
+		Text TEXT,
+		CMC INTEGER,
+		Power INTEGER,
+		Toughness INTEGER,
+		SetId INTEGER,
+		Number TEXT, -- this is text as there can be letters in teh card number
+		Id TEXT,
+		ArtistId INTEGER,
+		RarityId INTEGER,
+		LayoutId INTEGER,
+		
+		
+		CONSTRAINT Unique_Card
+		UNIQUE (
+			Name,
+			Text,
+			CMC,
+			Power,
+			Toughness,
+			SetId,
+			Number,
+			Id,
+			ArtistId,
+			RarityId,
+			LayoutId
+		)
+	);
+	")
+
+	# Create card layout table
+	db.execute("CREATE TABLE IF NOT EXISTS Layout(
+		LayoutId INTEGER PRIMARY KEY ASC,
+		Layout TEXT NOT NULL,
+		CONSTRAINT Unique_Layout
+		UNIQUE(Layout)
+	);")
+
+	# Create card rarity table
+	db.execute("CREATE TABLE IF NOT EXISTS Rarity(
+		RarityId INTEGER PRIMARY KEY ASC,
+		Rarity TEXT NOT NULL,
+		CONSTRAINT Unique_Rarity
+		UNIQUE(Rarity)
+	);")
+
+	# Create card artist table
+	db.execute("CREATE TABLE IF NOT EXISTS Artist(
+		ArtistId INTEGER PRIMARY KEY ASC,
+		Artist TEXT NOT NULL,
+		CONSTRAINT Unique_Artist
+		UNIQUE(Artist)
+	);")
+		
+# Create supertype tables	
+	# Create card supertype table
+	db.execute("CREATE TABLE IF NOT EXISTS Supertype(
+		SupertypeId INTEGER PRIMARY KEY ASC,
+		Supertype TEXT NOT NULL,
+		CONSTRAINT Unique_Supertype
+		UNIQUE(Supertype)
+	);")
+
+	# Create the supertype linking table
+	db.execute("CREATE TABLE IF NOT EXISTS CardSupertype (
+		CardSupertypeId INTEGER PRIMARY KEY ASC,
+		SupertypeId INTEGER NOT NULL,
+		CardId INTEGER NOT NULL,
+		CONSTRAINT Unique_CardSupertype
+		UNIQUE (SupertypeId, CardId)	
+	);")
+
+	
+# Create type tables	
+	# Create card type table
+	db.execute("CREATE TABLE IF NOT EXISTS Type(
+		TypeId INTEGER PRIMARY KEY ASC,
+		Type TEXT NOT NULL,
+		CONSTRAINT Unique_Type
+		UNIQUE(Type)
+	);")
+
+	# Create the type linking table
+	db.execute("CREATE TABLE IF NOT EXISTS CardType (
+		CardTypeId INTEGER PRIMARY KEY ASC,
+		TypeId INTEGER NOT NULL,
+		CardId INTEGER NOT NULL,
+		CONSTRAINT Unique_CardType
+		UNIQUE (TypeId, CardId)	
+	);")
+
+# Create subtype tables	
+	# Create card subtype table
+	db.execute("CREATE TABLE IF NOT EXISTS Subtype(
+		SubtypeId INTEGER PRIMARY KEY ASC,
+		Subtype TEXT NOT NULL,
+		CONSTRAINT Unique_Subtype
+		UNIQUE(Subtype)
+	);")
+		
+	# Create the subtype linking table
+	db.execute("CREATE TABLE IF NOT EXISTS CardSubtype (
+		CardSubtypeId INTEGER PRIMARY KEY ASC,
+		SubtypeId INTEGER NOT NULL,
+		CardId INTEGER NOT NULL,
+		CONSTRAINT Unique_CardSubtype
+		UNIQUE (SubtypeId, CardId)	
+	);")
+	
+# Create color tables	
+	# Create color table
+	db.execute("CREATE TABLE IF NOT EXISTS Color(
+		ColorId INTEGER PRIMARY KEY ASC,
+		Color TEXT NOT NULL,
+		Symbol TEXT NOT NULL,
+		CONSTRAINT Unique_Color
+		UNIQUE(Color, Symbol))
+	;")
+	
+	db.execute("INSERT INTO Color (Color, Symbol) VALUES ('Green', 'G');")
+	db.execute("INSERT INTO Color (Color, Symbol) VALUES ('Blue', 'U');")
+	db.execute("INSERT INTO Color (Color, Symbol) VALUES ('Black', 'B');")
+	db.execute("INSERT INTO Color (Color, Symbol) VALUES ('Red', 'R');")
+	db.execute("INSERT INTO Color (Color, Symbol) VALUES ('White', 'W');")
+	
+
+	# Create color linking table
+	db.execute("CREATE TABLE IF NOT EXISTS CardColor (
+		CardColorId INTEGER PRIMARY KEY ASC,
+		ColorId INTEGER NOT NULL,
+		CardId INTEGER NOT NULL,
+		CONSTRAINT Unique_CardColor
+		UNIQUE (ColorId, CardId)
+	);")
+end
+
+############
+# Finished #
+############
+
+puts "\n\nFinished."
---
+++ databases/InsertCardsIntoTables.rb
@@ -0,0 +1,252 @@
+# Eric Olson (c) 2016
+# Parse the JSON file containing infomation about 
+# the Magic The Gathering cards into databases
+
+require 'JSON'
+require 'sqlite3'
+DATABASE_LOCATION = ''
+cardInformationDb = DATABASE_LOCATION + 'CardInformation.db'
+	
+#####################################
+# Insert the card into the database #
+#####################################
+
+# Insert a card into the database or update it if it already exists
+#	db: the database connection to use
+#	setId: the id for the set that the card belongs to
+#	card: a json object used from MTGJson containing card information
+def InsertCard(db, setId, card)
+	name = card["name"]
+	text = card["text"]
+	cmc = card["cmc"]
+	power = card["power"]
+	toughness = card["toughness"]
+	number = card["number"]
+	id = card["id"]
+				
+	# create the card if it doesn't exist
+	if (GetCardId(db, setId, name, id).length == 0)
+		# Get ArtistId
+		artistId = GetValueId(db, "Artist", "ArtistId", "Artist", card["artist"])
+		
+		# Get RarityId
+		rarityId = GetValueId(db, "Rarity", "RarityId", "Rarity", card["rarity"])
+		
+		# Get LayoutId
+		layoutId = GetValueId(db, "Layout", "LayoutId", "Layout", card["layout"])
+		
+		db.execute("
+			INSERT INTO Card
+			(
+				Name,
+				Text,
+				CMC,
+				Power,
+				Toughness,
+				SetId,
+				Number,
+				Id,
+				ArtistId,
+				RarityId,
+				LayoutId
+			)
+			VALUES
+			(
+				?,	-- Name,
+				?,	-- Text,
+				?,	-- CMC,
+				?,	-- Power,
+				?,	-- Toughness,
+				?,	-- SetId,
+				?,	-- Number,
+				?,	-- Id
+				?,	-- ArtistId
+				?,	-- RarityId
+				?	-- LayoutId
+			);",
+			[
+				name,
+				text,
+				cmc,
+				power,
+				toughness,
+				setId,
+				number,
+				id,
+				artistId,
+				rarityId,
+				layoutId
+			])
+	end
+	
+	cardId = GetCardId(db, setId, name, id)
+		
+	# build the linking tables
+	InsertLinks(db, card["colors"], "CardColor", cardId, "Color", "ColorId", "Color")
+	InsertLinks(db, card["types"], "CardType", cardId, "Type", "TypeId", "Type")
+	InsertLinks(db, card["supertypes"], "CardSupertype", cardId, "Supertype", "SupertypeId", "Supertype")
+	InsertLinks(db, card["subtypes"], "CardSubtype", cardId, "Subtype", "SubtypeId", "Subtype")
+	
+	puts cardId
+end
+
+# Get the CardId for the card with the given information
+#	db: the database connection to use
+#	setId: the setId for the card
+#	id: the generated id for the card; it's a unique hash for that cards values and the set
+def GetCardId(db, setId, name, id)
+	return db.execute("
+		SELECT
+			CardId 
+		FROM 
+			Card
+		WHERE
+			Name = ? AND SetId = ? AND Id = ?
+		;",[
+			name, 
+			setId,
+			id,		
+		])
+end
+
+#################################################################################################
+# Create the linking tables for Colors, Types, Subtypes, Supertypes, Artists, Rarity and Layout #
+#################################################################################################
+
+# Return the Id from the table with the selected value if it exists. Insert value if it doesn't exist, then return the Id
+#	db: the database connection to use
+#	valueTable: the table which contains the value that is wanted
+#	idColumn: the column for the value table which contains the id of the item that is wanted
+#	valueColumn: the column that contains the value of the item that is wanted
+#	value:	the value that is wanted
+def GetValueId(db, valueTable, idColumn, valueColumn, value)
+	query = db.execute("SELECT " + idColumn + " FROM " + valueTable + " WHERE " + valueColumn + " = ?;",[value])
+	
+	if (query.length == 1)
+		return query
+	else
+		db.execute("INSERT INTO " + valueTable + "(" + valueColumn + ") VALUES (?);", [value])
+		return db.execute("SELECT " + idColumn + " FROM " + valueTable + " WHERE " + valueColumn + " = ?;",[value])
+	end
+end
+
+# Check linking table value
+#	db: the database connection to use
+#	linkingTable: the linking table to check
+#	valueIdColumn: the column which contains the value table's id
+#	valueId: the id for the value
+#	cardId: the id for the card the link is for
+def LinkingTableValueExists(db, linkingTable, valueIdColumn, valueId, cardId)
+	exists = db.execute("SELECT " + valueIdColumn.to_s + " FROM " + linkingTable + " WHERE CardId = ? AND " + valueIdColumn + " = ?;", [cardId, valueId])	
+	return exists.length != 0
+end
+
+# Create a link between the card and the value.
+#	linkValues: the values to create links for
+#	linkingTable: the name of the linking table
+#	cardId: the card to insert the link for
+#	valueTable: the table that contains the value to use in the linking table
+#	valueIdColumn: the column that contains the id of the value table and the column for it in the linking table
+#	valueColumn: the column that contains the value we need to check for in the value table to see if it exists; if not, we insert the value into the value table
+def InsertLinks(db, linkValues, linkingTable, cardId, valueTable, valueIdColumn, valueColumn)
+	if (linkValues == nil)
+		return
+	end
+	
+	linkValues.each do |linkValue|
+		linkValueId = GetValueId(db, valueTable, valueIdColumn, valueColumn, linkValue.to_s)
+		
+		if (!LinkingTableValueExists(db, linkingTable, valueIdColumn, linkValueId, cardId))
+			db.execute("INSERT INTO " + linkingTable + "(" + valueIdColumn +", CardId) VALUES (?,?);", [linkValueId, cardId])
+		end
+	end
+end
+
+
+## Insert the card colors into the CardColor linking table
+#def InsertCardColors(db, cardId, colors)
+#	if (colors == nil)
+#		return
+#	end
+#
+#	# Add the color for the card to the linking table if it doesn't exist
+#	colors.each do |color|
+#		colorId = db.execute("SELECT ColorId FROM Color WHERE Color = ?", color.to_s)
+#		
+#		if (!LinkingTableValueExists(db, "CardColor", "ColorId", colorId, cardId))
+#			db.execute("INSERT INTO CardColor (ColorId, CardId) VALUES (?, ?);",[colorId, cardId])
+#		end
+#	end
+#end
+
+##########################################
+# Parse the json file into sets of cards #
+##########################################
+
+puts "\n\nStarting..."
+puts "\n\n...\n\n"
+
+json = File.read('../rescources/AllSets-x.json')
+
+createdSets = 0
+createdCards = 0
+
+allSets = JSON.parse(json)
+SQLite3::Database.open(cardInformationDb) do |db|
+	allSets.each do |set|
+		
+		setId = -1
+			
+		# Attempt to insert the set
+		db.execute("
+			INSERT OR IGNORE INTO CardSet 
+					(Name, Code, ReleaseDate, Border, Type, Booster)
+			VALUES 	(?, 	?, 		?, 			?, 		?,		?)",[
+				set[1]['name'].to_s, 
+				set[1]['code'].to_s,
+				set[1]['releaseDate'].to_s,
+				set[1]['border'].to_s,
+				set[1]['type'].to_s,
+				set[1]['booster'].to_s
+			])
+		
+		# Grab the setId
+		setId = 
+			db.execute("
+				SELECT SetId FROM CardSet
+				WHERE Name = ? AND
+				CODE = ? AND
+				ReleaseDate = ? AND
+				Border = ? AND
+				TYPE = ? AND
+				Booster = ?", [
+				set[1]['name'].to_s, 
+				set[1]['code'].to_s,
+				set[1]['releaseDate'].to_s,
+				set[1]['border'].to_s,
+				set[1]['type'].to_s,
+				set[1]['booster'].to_s
+			] )[0]
+		
+		createdSets += 1
+		
+		
+		# Create each card in the set		
+		set[1]['cards'].each do |card|
+			InsertCard(db, setId, card)
+		
+			createdCards += 1
+		end
+		
+		
+		puts set[1]['name'].to_s + " - Cards: " + set[1]['cards'].length.to_s	
+	end
+end
+
+############
+# Finished #
+############
+
+puts "\nSets created: " + createdSets.to_s
+puts "Cards created: " + createdCards.to_s
+puts "\n\nFinished."
